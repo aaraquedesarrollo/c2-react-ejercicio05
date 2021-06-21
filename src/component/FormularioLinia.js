db0b6e8ba60113d248e8c617bbdc23f91f27b69a
Jose Maria Zamora Perez <jzamora@biada.es>
2021-06-21 17:49:16 +0200

    introducir los datos para llamar a la api de parada coger la linea deseada y hacer una request a api lineas

--- src/component/FormularioLinia.js
+++ src/component/FormularioLinia.js
@@ -1,8 +1,16 @@
-export const FormularioLinia = () => {
+import { useState } from "react";
+
+export const FormularioLinia = (props) => {
+  const { comprobarParada, parada, setParada } = props;
+
   return (
-    <form>
+    <form onSubmit={(e) => comprobarParada(e, parada)}>
       <label htmlFor="num-parada">Parada nº: </label>
-      <input type="number" id="num-parada" />
+      <input
+        type="number"
+        id="num-parada"
+        onChange={(e) => setParada(e.target.value)}
+      />
       <button type="submit">Buscar</button>
     </form>
   );
