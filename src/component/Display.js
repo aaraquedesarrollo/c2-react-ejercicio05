5bc0dfd2cdf5eb4817e4816851fa3501f3a5ee0b
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-21 22:45:05 +0200

    Componente display

--- src/component/Display.js
+++ src/component/Display.js
@@ -1,21 +1,15 @@
-export const Display = () => {
+export const Display = (props) => {
+  const { listaLineas } = props;
+
   return (
-<div className="display">
-          <div className="bus">
-            <span className="linea">V16</span>
-            <span className="destino">Universitat</span>
-            <span className="tiempo">10min</span>
-          </div>
-          <div className="bus">
-            <span className="linea">H12</span>
-            <span className="destino">Pla de Palau</span>
-            <span className="tiempo">1min</span>
-          </div>
-          <div className="bus">
-            <span className="linea">32</span>
-            <span className="destino">Barceloneta</span>
-            <span className="tiempo">4min</span>
-          </div>
+    <div className="display">
+      {listaLineas.map((linea) => (
+        <div key={linea.line} className="bus">
+          <span className="linea">{linea.line}</span>
+          <span className="destino">{linea.destination}</span>
+          <span className="tiempo">{linea["text-ca"]}</span>
         </div>
+      ))}
+    </div>
   );
 };
